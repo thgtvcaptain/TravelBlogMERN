114fcf38e0710577dd5b6703ca9bb51082aee53d
thgtvcaptain <thangtveh7031@gmail.com>
2023-01-03 17:47:16 +0700

    set some options
    set options for express app and mongoose to get rid of warning

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
-app.use(express.json({limit: '30mb', extend: true}));
-app.use(express.urlencoded({limit: '30mb', extend: true}));
+app.use(express.json({limit: '30mb', extended: true}));
+app.use(express.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
@@ -18,6 +18,7 @@ app.use('/posts', postRoutes);
 const dbURL = 'mongodb+srv://'+process.env.user+':'+process.env.password+'@cluster0.zv0i5nc.mongodb.net/mern?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
+mongoose.set('strictQuery', false);
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log("Listening on port " + PORT)))
     .catch((error) => console.log(error.message));
