d033584585d12b2c4df02a2c94470bab81bd60aa
thgtvcaptain <107764455+thgtvcaptain@users.noreply.github.com>
2023-02-09 10:08:59 +0700

    add auth form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Typography, Avatar, Button, Paper, Grid, Container } from "@mui/material";
+
+import LockOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
+
+import Input from './Input';
+
+import styles from './styles';
 
 const Auth = () => {
-    const state=null;
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
+
+    const handleChange = () => {}
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
+    const handleSubmit = () => {
+
+    }
+
+    const switchMode = () => {
+        setIsSignup(!isSignup);
+    }
 
     return ( 
-        <div>
-            AUTH
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper sx={styles.paper} elevation={3}>
+                <Avatar xs={styles.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form xs={styles.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {   isSignup && (
+                            <>
+                                <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half/>
+                                <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+                            </>    
+                            )}
+                            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword}/>
+                    </Grid>
+                    <Button type="submit" fullWidth variant="contained" color="primary" sx={styles.submit}>
+                        { isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                { isSignup ? 'Already have account? Sign in' : 'Dont have account? Sign up' }
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     );
 }
  
